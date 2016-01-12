60dd439d67a801459814e77df4ff1bbe84abe2b7
kevin <therealkevindavis@gmail.com>
2016-01-11 20:38:06 -0800

    Initial Commit
    This is the .Net code running on Mirror UI v1

---
+++ App_Code/GoogleCalendar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using Google.Apis.Calendar.v3.Data;
+
+/// <summary>
+/// Summary description for GoogleCalendar
+/// </summary>
+public static class GoogleCalendar
+{
+    private static string KeyFile
+    {
+        get
+        {
+            return HttpContext.Current.Server.MapPath("~/App_Data/MirrorUI.p12");
+        }
+    }
+    private static string ServiceAccount
+    {
+        get
+        {
+            return "SERVICE_ACCOUNT";
+        }
+    }
+    public static Events GetEvents()
+    {
+        Events events;
+
+            string[] scopes = new string[] {
+            CalendarService.Scope.Calendar, // Manage your calendars
+ 	        CalendarService.Scope.CalendarReadonly // View your Calendars
+            };
+
+            var certificate = new X509Certificate2(KeyFile, "notasecret", X509KeyStorageFlags.Exportable);
+
+            ServiceAccountCredential credential = new ServiceAccountCredential(
+                 new ServiceAccountCredential.Initializer(ServiceAccount)
+                 {
+                     Scopes = scopes
+                 }.FromCertificate(certificate));
+
+            // Create the service.
+            CalendarService service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "MirrorUI",
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("fake@gmail.com");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+        // List events
+        events = request.Execute();
+        return events;
+    }
+}
---
+++ App_Code/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Utils
+/// </summary>
+public static class Utils
+{
+    public static string Ordinal(this int number)
+    {
+        const string TH = "th";
+        var s = number.ToString();
+
+        number %= 100;
+
+        if ((number >= 11) && (number <= 13))
+        {
+            return s + TH;
+        }
+
+        switch (number % 10)
+        {
+            case 1:
+                return s + "st";
+            case 2:
+                return s + "nd";
+            case 3:
+                return s + "rd";
+            default:
+                return s + TH;
+        }
+    }
+}
